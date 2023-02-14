9f5799befcbf59dd0e406b88c2fd829346dd3b59
DominoxB <dominika-biedulska@tlen.pl>
2023-02-14 14:18:56 +0100

    feat: add repository

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <button class="bg-lime-800 w-10 h-10">czy</button>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
