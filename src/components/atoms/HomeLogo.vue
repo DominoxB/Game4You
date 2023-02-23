d5d1f2faeccca74b2421468a7f85624fefb979ca
DominoxB <dominika-biedulska@tlen.pl>
2023-02-23 15:23:42 +0100

    feat: add dropdown and HomeContent

--- src/components/atoms/HomeLogo.vue
+++ src/components/atoms/HomeLogo.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="mx-12 pt-4 bg-white px-3">
+  <div class="mx-12 pt-4 bg-white px-3 rounded">
     <span class="text-3xl font-kanit">Domino Games</span>
-    <img src="@/components/icons/domino-brick.jpg" class="w-20 h-12 mx-auto pt-2 rounded" alt="domino" />
+    <img src="@/components/icons/domino-brick.jpg" class="w-20 h-12 mx-auto py-2" alt="domino" />
   </div>
 </template>
 
---
+++ src/views/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <HomeContent />
+</template>
+
+<script>
+import HomeContent from '@/components/atoms/HomeContent.vue'
+export default {
+  name: 'HomePage',
+  components: {
+    HomeContent
+}
+}
+</script>
