0568f0b3e9a531b9bc533b0a0fc7e8e0f27f2000
Dominika <dominika-biedulska@tlen.pl>
2023-12-05 13:28:18 +0100

    fix: add changes in navbar menu

--- src/components/molecules/NavbarMenu.vue
+++ src/components/molecules/NavbarMenu.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="flex justify-center bg-gray-900">
-    <div class="flex items-center text-white font-kanit space-x-8 mt-8 cursor-pointer uppercase">
+  <div class="flex justify-center items-center bg-gray-900 h-32 text-white font-kanit">
+    <div class="flex space-x-8 cursor-pointer uppercase">
       <Dropdown title="Nasze Gry" :items="games" />
-      <span class="p-3 border-b-2 border-solid border-transparent hover:bg-gray-700 hover:border-b-pink-900 text-md">O
+      <span class="border-b-2 p-2 border-solid border-transparent hover:border-b-pink-900 text-md">O
         nas</span>
     </div>
     <router-link to="/">
       <HomeLogo />
     </router-link>
-    <div class="flex p-3 items-center text-md text-white font-kanit space-x-8 mt-8 cursor-pointer uppercase text-md">
+    <div class="flex text-md space-x-8 cursor-pointer uppercase">
       <span
-        class="p-3 border-b-2 border-solid border-transparent hover:bg-gray-700 hover:border-b-pink-900">Współpraca</span>
+        class="p-2 border-b-2 border-solid border-transparent hover:border-b-pink-900">Współpraca</span>
       <span
-        class="p-3 border-b-2 border-solid border-transparent hover:bg-gray-700 hover:border-b-pink-900">Kontakt</span>
+        class="p-2 border-b-2 border-solid border-transparent hover:border-b-pink-900">Kontakt</span>
     </div>
   </div>
 </template>
