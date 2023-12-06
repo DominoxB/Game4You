4e0be1df6e566ad14c3dc3cbcd8d3d2070bde0c6
Dominika <dominika-biedulska@tlen.pl>
2023-12-06 12:10:52 +0100

    feat: add props image

--- src/components/atoms/CardItem.vue
+++ src/components/atoms/CardItem.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="text-white border w-[120px] h-[120px]" @click="isVisible = !isVisible">
-      <div v-if="isVisible">Obrazek</div>
+  <div class="text-white border w-[120px] h-[120px] flex items-center justify-center" @click="isVisible = !isVisible">
+      <div v-if="isVisible">{{ image }}</div>
   </div>
 </template>
 
@@ -8,6 +8,12 @@
 import { defineComponent, ref } from 'vue'
 export default defineComponent({
   name: 'CardItem',
+  props: {
+    image: {
+      type: String,
+      required: true
+    }
+  },
   setup() {
     const isVisible = ref(false)
     return {
--- src/views/MemoryGame.vue
+++ src/views/MemoryGame.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <CardItem />
+    <CardItem image="😍"/>
   </div>
 </template>
 
