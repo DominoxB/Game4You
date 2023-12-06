998f5634dd1e51342b1c95b4636cea53459f79ab
Dominika <dominika-biedulska@tlen.pl>
2023-12-06 07:58:15 +0100

    feat: add style

--- src/components/atoms/HomeContent.vue
+++ src/components/atoms/HomeContent.vue
@@ -1,20 +1,19 @@
 <template>
-  <div class="mx-auto h-screen mt-10 bg-gray-900 my-auto">
-    <div class="my-auto text-center p-8 text-white">
-      <span class="text-xl font-dosis leading-10">Domino Games to miejsce dla ludzi, którzy cenią sobie gry - klasyki,
-        takie jak memory! Sprawdź swoją pamięć, spróbuj szczęścia i znajdź wszystkie pary! A może wolisz zagrać w kółko i
-        krzyżyk?
-        Pamiętaj - tylko trzy takie same znaki w linii zaprowadzą Cię do zwycięstwa. Powodzenia!</span>
+  <div class="h-screen bg-gray-900 my-auto">
+    <div class="text-center mt-20 mx-80 pb-10 text-white">
+      <span class="text-2xl font-dosis leading-10">Game4You to miejsce dla ludzi, którzy cenią sobie kultowe gry-klasyki.
+        Sprawdź swoją pamięć, spróbuj szczęścia i znajdź wszystkie pary w grze pamięciowej Memory Doggy! A może wolisz zagrać w kółko i
+        krzyżyk? Pamiętaj - tylko trzy takie same znaki w linii zaprowadzą Cię do zwycięstwa. Powodzenia!</span>
     </div>
-    <div class="grid grid-cols-2 mt-10">
+    <div class="flex justify-center space-x-24 mt-20">
       <router-link to="/MemoryPage">
-        <div class="flex justify-center transition lease-in-out delay-100 hover:scale-110 cursor-pointer z-0">
+        <div class="transition lease-in-out delay-100 hover:scale-110 cursor-pointer z-0">
           <img src="@/components/images/memory-game.png" alt="Memory Game"
             class="w-[600px] h-[300px] border-4 border-white rounded" />
         </div>
       </router-link>
       <router-link to="/TicTacToePage">
-        <div class="flex justify-center transition lease-in-out delay-100 hover:scale-110 cursor-pointer z-0">
+        <div class="transition lease-in-out delay-100 hover:scale-110 cursor-pointer z-0">
           <img src="@/components/images/tictactoe.png" alt="Kółko i krzyżyk"
             class="w-[600px] h-[300px] border-4 border-white rounded" />
         </div>
