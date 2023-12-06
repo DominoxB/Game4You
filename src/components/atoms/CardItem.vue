55c43ce7ee9d5e9e0e3ae51911e736a53d7a5081
Dominika <dominika-biedulska@tlen.pl>
2023-12-06 11:10:43 +0100

    feat: create new views

---
+++ src/components/atoms/CardItem.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="text-white">Card</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'CardItem'
+})
+</script>
---
+++ src/views/MemoryGame.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <CardItem />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import CardItem from '@/components/atoms/CardItem.vue'
+export default defineComponent({
+  name: 'MemoryGame',
+  components: {
+    CardItem
+  }
+})
+</script>
