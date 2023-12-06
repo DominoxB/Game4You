458bf276d5ee9caefd1bb837141db61c6e223870
Dominika <dominika-biedulska@tlen.pl>
2023-12-06 11:10:28 +0100

    feat: add new paths

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router"
 import HomePage from '@/views/HomePage.vue'
+import MemoryGame from "@/views/MemoryGame.vue"
+import TicTacToeGame from "@/views/TicTacToeGame.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: "/", component: HomePage }
+    { path: "/", component: HomePage },
+    { path: "/MemoryGame", name: 'Memory', component: MemoryGame },
+    { path: "/TicTacToeGame", name: 'TicTac', component: TicTacToeGame }
   ],
 });
 
