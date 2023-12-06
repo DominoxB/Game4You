f4a7d5df32d868a35c50fba33066dbe7974ed866
Dominika <dominika-biedulska@tlen.pl>
2023-12-06 07:58:35 +0100

    fix: remove absolute

--- src/components/atoms/FooterContent.vue
+++ src/components/atoms/FooterContent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="absolute inset-x-0 bottom-0 text-right pr-4 my-auto text-white">
+  <div class="inset-x-0 bottom-0 text-right pr-4 text-white">
     <span>&copy;Domino IT 2023</span>
   </div>
 </template>
