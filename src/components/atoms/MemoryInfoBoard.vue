d6fab27265c964d9aba4fd2855edf4d00dc4d5ff
Dominika <dominika-biedulska@tlen.pl>
2023-12-07 14:10:05 +0100

    feat: build stopwatch timer

--- src/components/atoms/MemoryInfoBoard.vue
+++ src/components/atoms/MemoryInfoBoard.vue
@@ -3,13 +3,17 @@
     class="flex space-x-8 justify-center mb-10 border-4 p-6 rounded-xl border-t-cyan-500 border-b-pink-400 border-x-lime-500 text-lime-200 text-xl">
     <div class="border border-indigo-800 px-2 py-1 rounded-md bg-slate-800">Błędy: 5</div>
     <div class="border border-indigo-800 px-2 py-1 rounded-md bg-slate-800">Ruchy: 10</div>
-    <div class="border border-indigo-800 px-2 py-1 rounded-md bg-slate-800">Czas</div>
+    <Timer />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Timer from './Timer.vue'
 export default defineComponent({
-  name: 'MemoryInfo'
+  name: 'MemoryInfo',
+  components: {
+    Timer
+  }
 })
 </script>
---
+++ src/components/atoms/Timer.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="border border-indigo-800 px-2 py-1 rounded-md bg-slate-800">
+    <span>{{ formatTime(timeElapsed) }}</span>
+    <button class="ml-3" @click="start">START</button>
+  </div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent, ref } from "vue"
+
+export default defineComponent({
+  name: 'MemoryTimer',
+  setup() {
+    const timeElapsed = ref(0)
+    const count = () => {
+      setInterval(() => {
+        timeElapsed.value++
+      }, 1000)
+    }
+    const formatTime = (timeElapsed: number) => {
+     const minutes = `0${Math.floor(timeElapsed / 60)}`.slice(-2)
+     const seconds = `0${timeElapsed % 60}`.slice(-2)
+     return `${minutes}:${seconds}`
+    
+    }
+    const start = () => {
+      count()
+    }
+    return {
+      timeElapsed,
+      formatTime,
+      start
+    }
+  }
+})
+
+</script>
