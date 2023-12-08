9da2b58672bd08f0410f03481cde3e40ea476799
Dominika <dominika-biedulska@tlen.pl>
2023-12-08 13:59:09 +0100

    feat: add new font

--- src/components/atoms/CardItem.vue
+++ src/components/atoms/CardItem.vue
@@ -15,6 +15,10 @@ export default defineComponent({
     image: {
       type: String,
       required: true
+    },
+    visible: {
+      type: Boolean,
+      default: false
     }
   },
   setup() {
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
         dosis: ["Dosis", "sans-serif"],
         kanit: ["Kanit", "sans-serif"],
         bebas: ['Bebas Neue', 'sans-serif'],
-        silk: ['Silkscreen', 'sans-serif']
+        silk: ['Silkscreen', 'sans-serif'],
+        chacra: ['Chakra Petch', 'sans-serif']
       },
     }
   },
