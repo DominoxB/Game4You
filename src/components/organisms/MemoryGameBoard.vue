07f6737359e7f7cf979ac7a3e0dbdbd73905bff1
Dominika <dominika-biedulska@tlen.pl>
2023-12-12 10:57:31 +0100

    fix: rebuild component

--- src/components/organisms/MemoryGameBoard.vue
+++ src/components/organisms/MemoryGameBoard.vue
@@ -2,15 +2,18 @@
   <div>
     <div v-if="showBtn" class="font-silk text-center mb-8">
       <div class="text-white text-5xl p-2 bg-gradient-to-r from-sky-500 to-indigo-500">Memory Game</div>
-      <button class="border p-1 text-white text-3xl mt-8" @click="startGame">START</button>
+      <button class="border p-1 text-white text-3xl my-8" @click="startGame">START</button>
+      <div class="w-[480px] h-[480px] border border-indigo-300 text-white text-center">
+        TU BĘDZIE JAKIEŚ IMG GRY
+      </div>
     </div>
     <div v-else>
       <MemoryInfoBoard />
-      <Timer v-if="!showBtn" class="mx-auto" />
-    </div>
-    <div class="grid grid-cols-4 grid-rows-4 w-[480px] h-[480px]">
-      <div v-for="card in shuffledCards" :key="card.id">
-        <CardItem :card="card" />
+      <Timer class="mx-auto" />
+      <div class="grid grid-cols-4 grid-rows-4 w-[480px] h-[480px]">
+        <div v-for="card in shuffledCards" :key="card.id">
+          <CardItem :card="card" />
+        </div>
       </div>
     </div>
   </div>
@@ -39,7 +42,7 @@ export default defineComponent({
     const startGame = () => {
       showBtn.value = false
     }
-    
+
     return {
       cards,
       shuffledCards,
