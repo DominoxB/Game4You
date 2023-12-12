27b361a31b354812450b262ddf99dbf2c99477f3
Dominika <dominika-biedulska@tlen.pl>
2023-12-12 09:58:07 +0100

    feat: update store

--- src/stores/memoryStore.ts
+++ src/stores/memoryStore.ts
@@ -8,85 +8,101 @@ export const useMemoryStore = defineStore("Memory", {
         {
           id: 1,
           image: require('../components/images/happy.png'),
-          name: 'Happy'
+          name: 'Happy',
+          visible: false
         },
         {
           id: 2,
           image: require('../components/images/happy.png'),
-          name: 'Happy'
+          name: 'Happy',
+          visible: false
         },
         {
           id: 3,
           image: require('../components/images/dog.png'),
-          name: 'Dog'
+          name: 'Dog',
+          visible: false
         },
         {
           id: 4,
           image: require('../components/images/dog.png'),
-          name: 'Dog'
+          name: 'Dog',
+          visible: false
         },
         {
           id: 5,
           image: require('../components/images/doggy.png'),
-          name: 'Doggy'
+          name: 'Doggy',
+          visible: false
         },
         {
           id: 6,
           image: require('../components/images/doggy.png'),
-          name: 'Doggy'
+          name: 'Doggy',
+          visible: false
         },
         {
           id: 7,
           image: require('../components/images/shiba.png'),
-          name: 'Shiba'
+          name: 'Shiba',
+          visible: false
         },
         {
           id: 8,
           image: require('../components/images/shiba.png'),
-          name: 'Shiba'
+          name: 'Shiba',
+          visible: false
         },
         {
           id: 9,
           image: require('../components/images/golden-retriever.png'),
-          name: 'Golden'
+          name: 'Golden',
+          visible: false
         },
         {
           id: 10,
           image: require('../components/images/golden-retriever.png'),
-          name: 'Golden'
+          name: 'Golden',
+          visible: false
         },
         {
           id: 11,
           image: require('../components/images/dogs.png'),
-          name: 'Dogs'
+          name: 'Dogs',
+          visible: false
         },
         {
           id: 12,
           image: require('../components/images/dogs.png'),
-          name: 'Dogs'
+          name: 'Dogs',
+          visible: false
         },
         {
           id: 13,
           image: require('../components/images/bone.png'),
-          name: 'Bone'
+          name: 'Bone',
+          visible: false
         },
         {
           id: 14,
           image: require('../components/images/bone.png'),
-          name: 'Bone'
+          name: 'Bone',
+          visible: false
         },
         {
           id: 15,
           image: require('../components/images/pet.png'),
-          name: 'Pet'
+          name: 'Pet',
+          visible: false
         },
         {
           id: 16,
           image: require('../components/images/pet.png'),
-          name: 'Pet'
+          name: 'Pet',
+          visible: false
         } 
       ],
-      selectedCards: {} as string
+      selectedCards: [] as any
     }
   },
 })
