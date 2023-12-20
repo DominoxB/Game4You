526bb161e999326e8d1a0f87b657b27d2f9f91ad
Dominika <dominika-biedulska@tlen.pl>
2023-12-20 18:20:10 +0100

    feat: add memory's intro img

--- src/components/organisms/MemoryGameBoard.vue
+++ src/components/organisms/MemoryGameBoard.vue
@@ -3,9 +3,7 @@
     <div v-if="showBtn" class="font-silk text-center mb-8">
       <div class="text-white text-5xl p-2 bg-gradient-to-r from-sky-500 to-indigo-500">Memory Game</div>
       <button class="border p-1 text-white text-3xl my-8" @click="startGame">START</button>
-      <div class="w-[480px] h-[480px] border border-indigo-300 text-white text-center">
-        TU BĘDZIE JAKIEŚ IMAGE GRY
-      </div>
+      <img src="../images/memory.png" />
     </div>
     <div v-else>
       <MemoryInfoBoard />
