4310a107481777d81fb113e80d0898f5ae2f5ebe
Dominika <dominika-biedulska@tlen.pl>
2023-12-27 10:55:23 +0100

    feat: add watch to stop timer after game end

--- src/components/atoms/Timer.vue
+++ src/components/atoms/Timer.vue
@@ -1,36 +1,44 @@
 <template>
-  <div class="flex justify-center items-center w-1/3 font-silk border border-indigo-600 py-2 text-lime-200 rounded-md my-5 bg-gradient-to-r from-sky-500 to-indigo-500">
+  <div
+    class="flex justify-center items-center w-1/3 font-silk border border-indigo-600 py-2 text-lime-200 rounded-md my-5 bg-gradient-to-r from-sky-500 to-indigo-500">
     <span>{{ formatTime(timeElapsed) }}</span>
   </div>
 </template>
 
 
 <script lang="ts">
-import { defineComponent, ref, onMounted } from "vue"
+import { defineComponent, ref, watch } from "vue"
+import { storeToRefs } from 'pinia'
+import { useMemoryStore } from '@/stores/memoryStore'
+
 
 export default defineComponent({
   name: 'MemoryTimer',
   setup() {
+    const cardsStore = useMemoryStore()
+    const { showModal } = storeToRefs(cardsStore)
     const timeElapsed = ref(0)
-    const count = () => {
-      setInterval(() => {
-        timeElapsed.value++
-      }, 1000)
-    }
+
     const formatTime = (time: number) => {
-     const minutes = `0${Math.floor(time / 60)}`.slice(-2)
-     const seconds = `0${time % 60}`.slice(-2)
-     return `${minutes}:${seconds}`
-    
+      const minutes = `0${Math.floor(time / 60)}`.slice(-2)
+      const seconds = `0${time % 60}`.slice(-2)
+      return `${minutes}:${seconds}`
     }
-    onMounted(() => {
-      count()
-    })
+
+    const start = setInterval(() => {
+      timeElapsed.value++
+    }, 1000)
+
+    watch(showModal, () => {
+      if (showModal.value === true) {
+        clearInterval(start)
+      }
+    });
+
     return {
       timeElapsed,
       formatTime,
     }
   }
 })
-
 </script>
