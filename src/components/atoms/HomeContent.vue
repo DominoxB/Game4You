6fc0f2db9ab30de0c362d6fad7525190be521d02
Dominika <dominika-biedulska@tlen.pl>
2023-12-28 11:44:53 +0100

    fix: change font color

--- src/components/atoms/HomeContent.vue
+++ src/components/atoms/HomeContent.vue
@@ -5,22 +5,22 @@
         Sprawdź swoją pamięć, spróbuj szczęścia i znajdź wszystkie pary w grze pamięciowej Memory Doggy! A może wolisz zagrać w kółko i
         krzyżyk? Pamiętaj - tylko trzy takie same znaki w linii zaprowadzą Cię do zwycięstwa. Powodzenia!</span>
     </div>
-    <div class="flex justify-center text-center space-x-24 mt-16">
+    <div class="flex justify-center text-center text-blue-200 font-silk text-3xl space-x-24 mt-16">
       <router-link to="/MemoryGame">
         <div class="cursor-pointer w-[445px] h-[445px]" @mouseover="hover = true" @mouseleave="hover = false">
-          <span class="font-silk text-pink-400 text-3xl">Memory</span>
+          <span>Memory</span>
           <video width="445" height="445" autoplay v-if="hover" class="mt2">
             <source src="../videos/memory.mp4">
           </video>
           <img src="@/components/images/mems.png" alt="Memory Game"
-            class="border-4 border-pink-800 rounded mt-2" v-else/>
+            class="border-4 border-indigo-800 rounded mt-2" v-else/>
         </div>
       </router-link>
       <router-link to="/TicTacToeGame">
-        <div class="cursor-pointer">
-          <span class="font-silk text-pink-400 text-3xl">Kółko i krzyżyk</span>
+        <div class="cursor-pointer w-[445px] h-[445px]">
+          <span>Kółko i krzyżyk</span>
           <img src="@/components/images/tictactoe.png" alt="Kółko i krzyżyk"
-            class="w-[600px] h-[300px] border-4 border-white rounded mt-2"/>
+            class="border-4 border-indigo-800 rounded mt-2"/>
         </div>
       </router-link>
     </div>
