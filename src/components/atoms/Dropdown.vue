f2cffd48a741fc2b3dc0fa570873d184972f8467
Dominika <dominika-biedulska@tlen.pl>
2023-12-29 09:06:53 +0100

    feat: work with style

--- src/components/atoms/Dropdown.vue
+++ src/components/atoms/Dropdown.vue
@@ -1,9 +1,9 @@
 <template>
   <div ref="dropdown">
-    <div class="flex p-2 border-b-2 border-solid border-transparent hover:border-b-pink-900"
+    <div class="flex py-2 border-b-2 border-solid border-transparent hover:border-b-pink-900"
       @click="isOpen = !isOpen">
       <span>{{ title }}</span>
-      <svg viewBox="0 0 1030 638" width="10" class="flex ml-5">
+      <svg viewBox="0 0 1030 638" width="10" class="flex ml-4">
         <path d="M1017 68L541 626q-11 12-26 12t-26-12L13 68Q-3 49 6 24.5T39 0h952q24 0 33 24.5t-7 43.5z" fill="#FFF">
         </path>
       </svg>
