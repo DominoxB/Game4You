4896eb8968165f39148bb5fc79bf5ef4d67d7a5a
Dominika <dominika-biedulska@tlen.pl>
2024-01-04 11:26:22 +0100

    feat: build new component with one card

--- src/components/atoms/CardItemTicTacToe.vue
+++ src/components/atoms/CardItemTicTacToe.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="text-white border border-indigo-300 w-[120px] h-[120px] flex items-center justify-center">
+  <div class="text-white border border-indigo-300 w-[120px] h-[120px] flex items-center justify-center cursor-pointer" @click="$emit('clickCard')">
+    {{ cardValue }}
   </div>
 </template>
 
@@ -8,6 +9,12 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'CardItemTicTacToe',
+  props: {
+    cardValue: {
+      type: String,
+      default: ''
+    }
+  },
+  emits: ['clickCard'],
 })
 </script>
-
--- src/components/organisms/TicTacToeGameBoard.vue
+++ src/components/organisms/TicTacToeGameBoard.vue
@@ -3,28 +3,30 @@
     <div v-if="showBtn" class="font-silk text-center mb-8">
       <div class="text-white text-5xl p-2 bg-gradient-to-r from-sky-500 to-indigo-500">Kółko i krzyżyk</div>
       <button class="border p-1 text-white text-3xl my-8" @click="startGame">START</button>
-      <img src="../images/tictac.png" class="mx-auto w-[480px] h-[480px]"/>
+      <img src="../images/tictac.png" class="mx-auto w-[480px] h-[480px]" />
     </div>
     <div v-else>
       <MemoryInfoBoard />
       <Timer class="mx-auto" />
-      <TicTacToe />
+      <div class="w-[450px] h-[450px] text-indigo-500 grid grid-cols-3 grid-rows-3 pt-4 font-bold text-5xl">
+        <CardItemTicTacToe />
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
-import TicTacToe from '@/components/atoms/TicTacToe.vue'
 import Timer from '@/components/atoms/Timer.vue'
 import MemoryInfoBoard from '@/components/atoms/MemoryInfoBoard.vue'
+import CardItemTicTacToe from '@/components/atoms/CardItemTicTacToe.vue'
 
 export default defineComponent({
   name: 'TicTacToeGameBoard',
   components: {
-    TicTacToe,
     Timer,
-    MemoryInfoBoard
+    MemoryInfoBoard,
+    CardItemTicTacToe
   },
   setup() {
     const showBtn = ref(true)
