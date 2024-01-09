4505725348070124aa7000bc54d2b5fc25afa78b
Dominika <dominika-biedulska@tlen.pl>
2024-01-09 10:19:33 +0100

    feat: add props message

--- src/components/atoms/TicTacToeInfoBoard.vue
+++ src/components/atoms/TicTacToeInfoBoard.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     class="flex space-x-8 justify-center border border-sky-500 p-6 rounded-xl text-lime-200 text-xl uppercase font-silk">
-    <div class="border border-indigo-500 px-2 py-1.5 rounded-md bg-slate-800">tu cos bedzie</div>
+    <div class="px-2 py-1.5 rounded-md">{{ message }}</div>
   </div>
 </template>
 
@@ -10,5 +10,10 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'TicTacToeInfo',
+  props: {
+    message: {
+      type: String,
+    }
+  }
 })
 </script>
