b96f4eff8a170110ea179dde8251171410a3c34f
Dominika <dominika-biedulska@tlen.pl>
2024-01-10 12:40:01 +0100

    feat: add  path to 2players game

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import HomePage from '@/views/HomePage.vue'
 import MemoryGame from "@/views/MemoryGame.vue"
 import TicTacToeGame from "@/views/TicTacToeGame.vue"
 import AboutUs from "@/components/atoms/AboutUs.vue"
+import TwoPlayersGame from "@/views/TwoPlayersGame.vue"
 
 const router = createRouter({
   history: createWebHistory(),
@@ -10,6 +11,7 @@ const router = createRouter({
     { path: "/", component: HomePage },
     { path: "/MemoryGame", name: 'Memory', component: MemoryGame },
     { path: "/TicTacToeGame", name: 'TicTac', component: TicTacToeGame },
+    { path: "/TwoPlayers", name: 'TwoPlayers', component: TwoPlayersGame },
     { path: "/AboutUs", name: 'AboutUs', component: AboutUs }
   ],
 });
