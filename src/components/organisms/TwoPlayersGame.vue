3696aa72660f4abe9775674d8cc360892efe778c
Dominika <dominika-biedulska@tlen.pl>
2024-01-12 11:00:09 +0100

    feat: add 2 players game content

--- src/components/organisms/TwoPlayersGame.vue
+++ src/components/organisms/TwoPlayersGame.vue
@@ -1,15 +1,103 @@
 <template>
-  <div class="text-white text-2xl">2 graczy</div>
+  <div class="mt-10">
+    <div>
+      <audio hidden="true" ref="audioX">
+        <source src="../sounds/xwin.mp3" type="audio/mpeg">
+      </audio>
+      <audio hidden="true" ref="audioO">
+        <source src="../sounds/owin.mp3" type="audio/mpeg">
+      </audio>
+      <audio hidden="true" ref="audioDraw">
+        <source src="../sounds/draw.mp3" type="audio/mpeg">
+      </audio>
+    </div>
+    <TicTacToeInfoBoard :message="infoText" />
+    <div class="flex justify-center">
+      <RefreshArrows class="mt-8" @refresh="refreshGame" />
+      <ConfettiExplosion :particleCount="300" v-if="confetti" :duration="2300"
+        :colors="['#fff', '#FF00FF', '#50EBEC', '#7D0541']" />
+    </div>
+    <div class="w-[450px] h-[450px] text-indigo-500 grid grid-cols-3 grid-rows-3 mx-auto font-bold text-5xl mt-8">
+      <CardItemTicTacToe v-for="n in 9" :key="n" :id="n" @show-sign="selectField" :is-x="fieldX.includes(n)"
+        :is-o="fieldO.includes(n)" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
+import TicTacToeInfoBoard from '@/components/atoms/TicTacToeInfoBoard.vue'
+import RefreshArrows from '@/components/atoms/RefreshArrows.vue'
+import CardItemTicTacToe from '@/components/atoms/CardItemTicTacToe.vue'
+import ConfettiExplosion from 'vue-confetti-explosion'
 export default defineComponent({
   name: 'TwoPlayersGame',
   components: {
+    RefreshArrows,
+    TicTacToeInfoBoard,
+    CardItemTicTacToe,
+    ConfettiExplosion
   }, 
   setup() {
-    
+    const infoText = ref('Kółko i krzyżyk')
+    const fieldX = ref([] as number[])
+    const fieldO = ref([] as number[])
+    const isBlocked = ref(false)
+    const confetti = ref(false)
+    const lines = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9],
+      [1, 5, 9],
+      [3, 5, 7]
+    ]
+    const audioX = ref<HTMLAudioElement>()
+    // const audioO = ref<HTMLAudioElement>()
+    const audioDraw = ref<HTMLAudioElement>()
+
+
+    const refreshGame = () => {
+      fieldX.value = []
+      fieldO.value = []
+      infoText.value = 'Kółko i krzyżyk'
+      isBlocked.value = false
+      confetti.value = false
+    }
+
+    const selectField = (id: number) => {
+      if (isBlocked.value) {
+        return
+      }
+      fieldX.value.push(id)
+      const winX = lines.map(line => line.every(el => fieldX.value.includes(el))) // sprawdzam, czy x lub o ma 3 znaki w linii, jesli tak-przerywam gre
+      console.log('line:', winX)
+      const allId = fieldX.value.concat(fieldO.value)
+      console.log(allId)
+      if (winX.includes(true)) {
+        infoText.value = 'X wygrywa!'
+        confetti.value = true
+        isBlocked.value = true
+        audioX.value?.play()
+        return
+      }
+      if (allId.length === 9) {
+        infoText.value = 'Mamy remis!'
+        audioDraw.value?.play()
+      }
+    }
+
+    return {
+      infoText,
+      confetti,
+      fieldX,
+      fieldO,
+      refreshGame,
+      selectField
+    }
+
   }
 })
 </script>
