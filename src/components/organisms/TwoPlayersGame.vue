d44469d8c59b5e27f7159fa588583ed79bd5ad19
Dominika <dominika-biedulska@tlen.pl>
2024-01-12 10:10:33 +0100

    feat: add two players game

--- src/components/organisms/TwoPlayersGame.vue
+++ src/components/organisms/TwoPlayersGame.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="flex justify-center items-center mt-20 text-white">
-    TO GRA DLA 2 GRACZY
-  </div>
+  <div class="text-white text-2xl">2 graczy</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'TwoPlayersGame',
+  components: {
+  }, 
+  setup() {
+    
+  }
 })
 </script>
