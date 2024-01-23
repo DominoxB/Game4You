45c071c6ee3501cc8f6cbf5f63ba56826a48657d
Dominika <dominika-biedulska@tlen.pl>
2024-01-23 09:21:02 +0100

    feat: add footer position on mobile

--- src/components/atoms/FooterContent.vue
+++ src/components/atoms/FooterContent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mt-8 md:absolute inset-x-0 bottom-0 text-right pr-4 text-white">
+  <div class="pt-8 relative lg:absolute inset-x-0 bottom-0 text-right pr-4 text-white">
     <span>&copy; Domino IT {{ year }}</span>
   </div>
 </template>
