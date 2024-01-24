066a23f444082e52607e2ce766b63c3c18be6034
Dominika <dominika-biedulska@tlen.pl>
2024-01-24 14:51:44 +0100

    feat: add content

--- src/components/atoms/AboutUs.vue
+++ src/components/atoms/AboutUs.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="flex justify-center mt-20 space-x-16">
-    <img src="../images/mems.png" alt="memory" class="w-60 animate-flip-down" />
-    <img src="../images/tictactoe.png" alt="memory" class="w-60 animate-flip-up" />
+  <div class="text-gray-300 text-center mx-40 px-20 mt-20 py-10 font-silk text-2xl animate-fade">
+    <span>Jesteśmy wielbicielami klasycznych gier, które towarzyszą nam praktycznie od dzieciństwa. Teraz postanowiliśmy
+      wykorzystać nasze programistyczne umiejętności do stworzenia własnej wersji online! Mamy nadzieję, że gierki dadzą
+      Wam co
+      najmniej tyle frajdy, co nam tworzenie ich.
+    </span>
+    <p>Enjoy!</p>
   </div>
-    <div class="text-white text-center mx-40 px-20 mt-20 py-10 font-silk text-2xl animate-fade-right">
-      <span>Jesteśmy wielbicielami klasycznych gier, które towarzyszą nam praktycznie od dzieciństwa. Teraz postanowiliśmy
-        wykorzystać nasze programistyczne umiejętności do stworzenia własnej wersji online! Mamy nadzieję, że gierki dadzą Wam co
-        najmniej tyle frajdy, co nam tworzenie ich.
-      </span>
-      <p >Enjoy!</p>
-    </div>
+  <div class="text-white text-center mt-8 py-10 font-silk text-2xl animate-fade-right mx-auto">
+    <span>Sprawdź także naszego <a href="https://dominoxb.github.io/gra-wisielec/" target="_blank" class="underline text-lime-300">Wisielca online!</a>
+    </span>
+    <img src="../images/hangman.png" alt="hangman" />
+  </div>
 </template>
