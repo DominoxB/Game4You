76adb2ada4aab6762e15b817b90eb414121acc97
Dominika <dominika-biedulska@tlen.pl>
2024-01-24 14:54:53 +0100

    feat: add mobile classes

--- src/components/atoms/AboutUs.vue
+++ src/components/atoms/AboutUs.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-gray-300 text-center mx-40 px-20 mt-20 py-10 font-silk text-2xl animate-fade">
+  <div class="text-gray-300 text-center mx-10 md:mx-40 md:px-20 mt-20 py-10 font-silk text-xl md:text-2xl animate-fade">
     <span>Jesteśmy wielbicielami klasycznych gier, które towarzyszą nam praktycznie od dzieciństwa. Teraz postanowiliśmy
       wykorzystać nasze programistyczne umiejętności do stworzenia własnej wersji online! Mamy nadzieję, że gierki dadzą
       Wam co
@@ -7,7 +7,7 @@
     </span>
     <p>Enjoy!</p>
   </div>
-  <div class="text-white text-center mt-8 py-10 font-silk text-2xl animate-fade-right mx-auto">
+  <div class="text-white text-center mt-8 md:py-10 font-silk text-xl md:text-2xl animate-fade-right mx-auto">
     <span>Sprawdź także naszego <a href="https://dominoxb.github.io/gra-wisielec/" target="_blank" class="underline text-lime-300">Wisielca online!</a>
     </span>
     <img src="../images/hangman.png" alt="hangman" />
