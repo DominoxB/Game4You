32870a600d3b7a0c596caeb50ff2534651a40255
Dominika <dominika-biedulska@tlen.pl>
2024-01-25 13:20:45 +0100

    fix: change fonts

--- src/components/atoms/MemoryInfoBoard.vue
+++ src/components/atoms/MemoryInfoBoard.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex space-x-2 justify-center border border-sky-500 px-2 py-4 md:py-6 rounded-xl text-lime-200 md:text-xl uppercase font-silk">
+    class="flex space-x-2 justify-center border border-sky-500 px-2 py-4 md:py-6 rounded-xl text-lime-200 md:text-xl uppercase font-roboto">
     <div class="border border-indigo-500 px-1 md:px-2 py-1.5 rounded-md bg-slate-800">{{ $t('mistakes') }}: {{ mistakesCounter }}
     </div>
     <div class="border border-indigo-500 px-1 md:px-2 py-1.5 rounded-md bg-slate-800">{{ $t('moves') }}: {{ movesCounter }}</div>
