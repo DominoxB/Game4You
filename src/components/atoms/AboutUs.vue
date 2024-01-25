8a5eb423c5fc79a4803bd516b49ba1f9212e374e
Dominika <dominika-biedulska@tlen.pl>
2024-01-25 08:47:43 +0100

    fix: change content and work with margins

--- src/components/atoms/AboutUs.vue
+++ src/components/atoms/AboutUs.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="text-gray-300 text-center mx-10 md:mx-40 md:px-20 mt-20 py-10 font-silk text-xl md:text-2xl animate-fade">
-    <span>Jesteśmy wielbicielami klasycznych gier, które towarzyszą nam praktycznie od dzieciństwa. Teraz postanowiliśmy
-      wykorzystać nasze programistyczne umiejętności do stworzenia własnej wersji online! Mamy nadzieję, że gierki dadzą
+  <div class="text-gray-300 text-center mx-10 md:mx-40 md:px-20 mt-12 py-10 font-silk text-xl md:text-2xl animate-fade">
+    <span>Jesteśmy wielbicielami klasycznych gier, które towarzyszą nam praktycznie od dzieciństwa. Teraz chcemy
+      wykorzystać nasze programistyczne umiejętności do stworzenia własnej wersji gier online! Mamy nadzieję, że gierki dadzą
       Wam co
       najmniej tyle frajdy, co nam tworzenie ich.
     </span>
     <p>Enjoy!</p>
   </div>
-  <div class="text-white text-center mt-8 md:py-10 font-silk text-xl md:text-2xl animate-fade-right mx-auto">
+  <div class="text-white text-center my-8 md:py-10 font-silk text-xl md:text-2xl animate-fade-right mx-auto">
     <span>Sprawdź także naszego <a href="https://dominoxb.github.io/gra-wisielec/" target="_blank" class="underline text-lime-300">Wisielca online!</a>
     </span>
     <img src="../images/hangman.png" alt="hangman" />
