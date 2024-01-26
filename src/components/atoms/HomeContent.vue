436f2fd99d50e8319c07972a4d45b12eef119139
Dominika <dominika-biedulska@tlen.pl>
2024-01-26 13:29:10 +0100

    feat: add mb

--- src/components/atoms/HomeContent.vue
+++ src/components/atoms/HomeContent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="mb-4">
     <div class="flex items-center justify-center py-16 lg:py-10 md:pt-16 text-white">
       <span
         class="relative w-[max-content] font-silk text-xl text-lime-200 sm:text-2xl uppercase md:text-3xl lg:text-5xl before:absolute before:inset-0 before:animate-typewriter before:bg-gray-900 after:absolute after:inset-0 after:w-[0.125em] after:animate-caret after:bg-white">
