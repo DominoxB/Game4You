d0c843aa718b3548424f4708d941b75c1e6058ad
Dominika <dominika-biedulska@tlen.pl>
2024-01-26 10:18:10 +0100

    fix: change animate for hangman

--- src/components/atoms/AboutUs.vue
+++ src/components/atoms/AboutUs.vue
@@ -3,7 +3,7 @@
     <span>{{ $t('aboutUs') }}</span>
     <p>{{ $t('enjoy') }}</p>
   </div>
-  <div class="text-white text-center my-8 md:py-10 font-silk text-xl md:text-2xl animate-fade-right mx-auto">
+  <div class="text-white text-center my-8 md:py-10 font-silk text-xl md:text-2xl animate-fade mx-auto">
     <span>{{ $t('hangman1') }} <a href="https://dominoxb.github.io/gra-wisielec/" target="_blank" class="underline text-lime-300">{{ $t('hangman2') }}</a>
     </span>
     <img src="../images/hangman.png" alt="hangman" />
