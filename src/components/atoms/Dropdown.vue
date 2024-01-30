c612d6d086f49b7f3a10d0729ac9d3a4451ee330
Dominika <dominika-biedulska@tlen.pl>
2024-01-30 10:59:30 +0100

    feat: add dropdown width

--- src/components/atoms/Dropdown.vue
+++ src/components/atoms/Dropdown.vue
@@ -1,15 +1,15 @@
 <template>
-  <div ref="dropdown" class="relative">
+  <div ref="dropdown">
     <div
-      class="flex py-2 border-b-2 justify-center border-solid border-transparent hover:border-b-pink-900"
+      class="relative flex py-2 border-b-2 justify-center border-solid border-transparent hover:border-b-pink-900 w-[76px] md:w-[84px]"
       @click="isOpen = !isOpen">
       <span>{{ title }}</span>
-      <svg viewBox="0 0 1030 638" width="10" class="flex ml-2">
+      <svg viewBox="0 0 1030 638" class="flex w-2 md:w-2.5 ml-2">
         <path d="M1017 68L541 626q-11 12-26 12t-26-12L13 68Q-3 49 6 24.5T39 0h952q24 0 33 24.5t-7 43.5z" fill="#FFF">
         </path>
       </svg>
     </div>
-    <div v-if="isOpen" class="fixed bg-gray-800 rounded-b-2xl text-center">
+    <div v-if="isOpen" class="absolute bg-gray-800 rounded-b-2xl text-center">
       <div v-for="(item, i) in items" :key="i"
         class="py-2 px-4 border-b-2 border-solid border-transparent font-roboto text-xs md:text-sm hover:bg-gray-700 hover:last:rounded-b-2xl hover:border-b-pink-900">
         <router-link :to="item.url">
